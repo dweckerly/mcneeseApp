78a19b2bed73c685b9aa0514c77d103cccd5d6af
David <d.weckerly@gmail.com>
2018-03-29 09:29:03 -0500

    finished submit form for pdf

--- util/submitForm.util.php
+++ util/submitForm.util.php
@@ -1,5 +1,42 @@
 <?php
+if(!isset($_POST['submit'])){
+    header("Location: ../pdf/error.php");
+    exit();
+} else if(!isset($_POST['name']) || !isset($_POST['company'])){
+    header("Location: ../pdf/error.php");
+    exit();
+} else {
+    if(empty($_POST['name']) || empty($_POST['company'])) {
+        header("Location: ../pdf/error.php");
+        exit();
+    } else {
+        $id = $_POST['eid'];
+        include_once("/include/db.php");
+        $sql = "SELECT * FROM events WHERE ID = '$id'";
+        $result = mysqli_query($conn, $sql);
+        $rCheck = mysqli_num_rows($result);
+        if($rCheck == 0) {
+            header("Location: ../pdf/error.php");
+            exit();
+        } else {
+            $rows = mysqli_fetch_assoc($result);
+            session_start();
+            $_SESSION['eName'] = $rows['name'];
+            $_SESSION['location'] = $rows['location'];
+            $_SESSION['date'] = $rows['date'];
+            $_SESSION['time'] = $rows['time'];
+            $_SESSION['name'] = mysqli_real_escape_string($conn, $_POST['name']);
+            $_SESSION['company'] = mysqli_real_escape_string($conn, $_POST['company']);
 
-// need to add person to DB
-// need to get event info
-// can use the session to pass to pdf
+            $name = mysqli_real_escape_string($conn, $_POST['name']);
+            $company = mysqli_real_escape_string($conn, $_POST['company']);
+            $event = $rows['name'];
+
+            $sql ="INSERT INTO attendees (name, company, event, eid) VALUES ('$name', '$company', '$event', '$id')";
+            mysqli_query($conn, $sql);
+            mysqli_close($conn);
+            header("Location: ../form/pdf/parkingPass.php");
+            exit();
+        }
+    }
+}
